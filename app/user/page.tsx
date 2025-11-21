27692aa730ac0876580607f26376936e2b278eb5
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-11-21 16:51:10 +0700

    add desc

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -4,6 +4,12 @@ export default function UserPage() {
   return (
     <div className="max-w-7xl h-full mx-auto px-4">
       <h1>User</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        reprehenderit alias error eos, id tempore non molestiae ab recusandae
+        consequuntur repudiandae sit unde, sequi exercitationem adipisci nobis
+        nisi obcaecati aliquam?
+      </p>
     </div>
   );
 }
