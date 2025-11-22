3f62195f07db63b46417224780f338a13d360a3b
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-11-22 14:26:31 +0700

    cache dynamic and static

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,15 +1,27 @@
-"use client";
+import { cacheLife } from "next/cache";
 
-export default function UserPage() {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default async function UserPage() {
+  "use cache";
+  cacheLife("seconds");
+
+  const data = await fetch(
+    "https://690da16aa6d92d83e8525a6e.mockapi.io/api/v1/user"
+  );
+  const users = (await data.json()) as any[];
+
   return (
     <div className="max-w-7xl h-full mx-auto px-4">
-      <h1>User</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-        reprehenderit alias error eos, id tempore non molestiae ab recusandae
-        consequuntur repudiandae sit unde, sequi exercitationem adipisci nobis
-        nisi obcaecati aliquam?
-      </p>
+      {users.map((user: { id: number; name: string; bio: string }) => (
+        <div
+          key={user.id}
+          className="mt-4 text-lg font-semibold text-blue-600 hover:text-blue-800 
+          cursor-pointer transition-colors hover:bg-blue-100 p-2 rounded border border-blue-200"
+        >
+          <h1 className="text-lg font-semibold">{user.name}</h1>
+          <p className="text-sm text-gray-600">{user.bio}</p>
+        </div>
+      ))}
     </div>
   );
 }
